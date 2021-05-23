3048556f5212773ca80008d62e535d824084e5f5
CodeMoreira <gabrielmoreirailustra@gmail.com>
2021-05-23 02:26:13 -0300

    create: first commit

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  bracketSpacing: false,
+  tsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import PhoneInputRn from './src/react-native-phone-input-br/index';
+
+const App = () => {
+  const [value, setValue] = useState<string>('');
+  const [phoneFormated, setPhoneFormated] = useState<string>('');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Phone Input React Native</Text>
+      <PhoneInputRn
+        value={value}
+        onChangeText={(text) => setValue(text)}
+        setPhoneFormated={setPhoneFormated}
+        cellFormat
+      />
+      <Text style={{fontSize: 20}}>Phone Formated: {phoneFormated}</Text>
+      <Text style={{fontSize: 20}}>Phone Value: {value}</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default App;
